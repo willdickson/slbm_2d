Fixed issue in corrector with incorrect sign.

scripts/plot_data.py
@@ -8,10 +8,11 @@ data = np.load(filename)
 
 #data = data[:,:,::-1]
 data = np.transpose(data,[0, 2, 1])
+n = 4
 
-p  = data[0,:,:]
-ux = data[1,:,:]
-uy = data[2,:,:]
+p  = data[0,::n,::n]
+ux = data[1,::n,::n]
+uy = data[2,::n,::n]
 
 fig, ax = plt.subplots(1,1)
 ax.quiver(ux,uy)