6bb63fca44acbb4d2e50f18834fbf31bdbdb3eef
willdickson <wbd@caltech.edu>
2022-12-15 11:40:49 -0800

    Modified config for testing IBM.

--- scripts/plot_data.py
+++ scripts/plot_data.py
@@ -8,7 +8,7 @@ data = np.load(filename)
 
 #data = data[:,:,::-1]
 data = np.transpose(data,[0, 2, 1])
-n = 1
+n = 1 
 
 p  = data[0,::n,::n]
 ux = data[1,::n,::n]
@@ -21,33 +21,34 @@ x, y = np.meshgrid(x,y)
 
 
 fig, ax = plt.subplots(1,1)
-if 1:
-    ax.pcolor(x,y,ux, cmap='RdYlBu')
 if 0:
+    ax.pcolor(x,y,w, cmap='RdYlBu')
+if 1:
     w0 = np.absolute(w).max()
-    s = 0.05
+    s = 0.1
     ax.pcolor(x,y,w, cmap='RdYlBu', vmin=-s*w0, vmax=s*w0)
-    ax.streamplot(x, y,ux,uy,color='k',linewidth=1.0, density=0.2)
+    ax.streamplot(x, y,ux,uy,color='k',linewidth=1.0, density=1.6)
 
     #ind = 100*np.arange(20)
     #ax.quiver(x[:,ind],y[:,ind],ux[:,ind],uy[:,ind])
     #print(ux[:,ind])
 
 if 0:
-    ax.quiver(x,y,ux,uy,scale=1.0)
+    ax.quiver(x,y,ux,uy,scale=10.0)
 
 if 0:
     for i in np.arange(20):
         s = 500
         y = np.arange(ux.shape[0])
         x0 = i*100
-        x = x0 + s*ux[:,100*i]
+        k = ux.shape[1]//20
+        print(k)
+        x = x0 + s*ux[:,k*i]
         y = y[1:-1]
         x = x[1:-1]
     
-        if i == 0:
-            ax.plot([x0, x[0]], [y[0], y[0]], 'b')
-            ax.plot([x0, x[0]], [y[-1],y[-1]], 'b')
+        ax.plot([x0, x[0]], [y[0], y[0]], 'b')
+        ax.plot([x0, x[0]], [y[-1],y[-1]], 'b')
         ax.plot(x,y,'b')
 ax.axis('scaled')
 plt.show()
